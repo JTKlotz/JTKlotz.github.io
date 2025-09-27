92b6bd158b4325b838be126bd956daef4c5d783c
Jordan Klotz <jordanklotz@Jordans-MacBook-Air.local>
2025-09-27 16:48:18 -0500

    Added carousel

--- site.js
+++ site.js
@@ -1,5 +1,43 @@
 
-//console.log("JavaScript is linked properly!");
+const prevButton = document.querySelector('#prev')
+const nextButton = document.querySelector('#next')
+
+const urls = [
+    'https://images.pexels.com/photos/1454360/pexels-photo-1454360.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    'https://images.pexels.com/photos/933964/pexels-photo-933964.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    'https://images.pexels.com/photos/267885/pexels-photo-267885.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    'https://images.pexels.com/photos/1251861/pexels-photo-1251861.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    'https://images.pexels.com/photos/1370296/pexels-photo-1370296.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+].map(url => { (new Image()).src = url; return url })
+
+const images = document.querySelectorAll('#carousel img')
+
+let currentImage = 0
+const showImages = () => {
+    const offset = currentImage % urls.length
+    images.forEach((image, index) => {
+        const imageIndex = (index + offset + urls.length) % urls.length
+        image.src = urls[imageIndex]
+    })
+}
+
+setInterval(() => {
+    currentImage++
+    showImages()
+}, 5000)
+
+prevButton.addEventListener('click', () => {
+    currentImage--
+    showImages()
+})
+
+nextButton.addEventListener('click', () => {
+    currentImage++
+    showImages()
+})
+
+showImages()
+
 
 //variable storing the welcome div
 const welcome = document.querySelector('#welcome')
